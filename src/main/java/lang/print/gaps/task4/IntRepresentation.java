89675ac1581152c7a66b11a82d5808258ed5a817
i.shirinov <shirinovilyos6965@gmail.com>
2022-11-21 16:48:50 +0500

    №4 vazifa. Primitivlar

--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,8 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        int a = 99, s = 104, d = 97, f = 114;
+
+        System.out.println((char) a + "" + (char) s + "" + (char) d + "" + (char) f);
     }
 }
